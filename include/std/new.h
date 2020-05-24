1062f757fabb9aaae6c0f633b84682d6168be018
Andrew Countryman <apcountryman@gmail.com>
2020-05-24 13:21:25 -0700

    Add array placement new/delete
    Supports #28.

--- include/std/new.h
+++ include/std/new.h
@@ -36,4 +36,13 @@ inline void operator delete(void *, void *)noexcept
 {
 }
 
+inline void * operator new[]( std::size_t, void * location ) noexcept
+{
+    return location;
+}
+
+inline void operator delete[]( void *, void * ) noexcept
+{
+}
+
 #endif // STD_NEW_H

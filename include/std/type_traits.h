86e91a2a1d6062f26057b91cb8c0c99d3aacdba8
Andrew Countryman <apcountryman@gmail.com>
2020-06-14 14:20:01 -0700

    Remove unnecessary use of std::
    Supports #34.

--- include/std/type_traits.h
+++ include/std/type_traits.h
@@ -309,7 +309,7 @@ template<typename T>
 struct remove_extent<T[]> : type_identity<T> {
 };
 
-template<typename T, std::size_t N>
+template<typename T, size_t N>
 struct remove_extent<T[ N ]> : type_identity<T> {
 };
 

2046bf15e9d99cf6a1df0a41dd848208dce110bc
Andrew Countryman <apcountryman@gmail.com>
2020-06-20 12:56:01 -0700

    Add std::forward
    Supports #35.

--- include/std/utility.h
+++ include/std/utility.h
@@ -27,6 +27,18 @@
 namespace std {
 
 template<typename T>
+constexpr auto forward( std::remove_reference_t<T> && t ) noexcept
+{
+    return static_cast<T &&>( t );
+}
+
+template<typename T>
+constexpr auto forward( std::remove_reference_t<T> & t ) noexcept
+{
+    return static_cast<T &&>( t );
+}
+
+template<typename T>
 constexpr auto move( T && t ) noexcept
 {
     return static_cast<std::remove_reference_t<T> &&>( t );

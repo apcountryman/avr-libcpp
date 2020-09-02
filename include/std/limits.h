2774487dc3f4ccb3799034dea11895f8dc51c933
Andrew Countryman <apcountryman@gmail.com>
2020-09-02 15:50:55 -0700

    Add std::numeric_limits<bool>
    Supports #49.

--- include/std/limits.h
+++ include/std/limits.h
@@ -132,6 +132,100 @@ struct numeric_limits {
     }
 };
 
+template<>
+struct numeric_limits<bool> {
+    static constexpr auto is_specialized = true;
+
+    static constexpr auto is_signed = false;
+
+    static constexpr auto is_integer = true;
+
+    static constexpr auto is_exact = true;
+
+    static constexpr auto has_infinity = false;
+
+    static constexpr auto has_quiet_NaN = false;
+
+    static constexpr auto has_signaling_NaN = false;
+
+    static constexpr auto has_denorm = denorm_absent;
+
+    static constexpr auto has_denorm_loss = false;
+
+    static constexpr auto round_style = round_toward_zero;
+
+    static constexpr auto is_iec559 = false;
+
+    static constexpr auto is_bounded = true;
+
+    static constexpr auto is_modulo = false;
+
+    static constexpr auto digits = 1;
+
+    static constexpr auto digits10 = 0;
+
+    static constexpr auto max_digits10 = 0;
+
+    static constexpr auto radix = 2;
+
+    static constexpr auto min_exponent = 0;
+
+    static constexpr auto min_exponent10 = 0;
+
+    static constexpr auto max_exponent = 0;
+
+    static constexpr auto max_exponent10 = 0;
+
+    static constexpr auto traps = false;
+
+    static constexpr auto tinyness_before = false;
+
+    static constexpr auto min() noexcept
+    {
+        return false;
+    }
+
+    static constexpr auto lowest() noexcept
+    {
+        return false;
+    }
+
+    static constexpr auto max() noexcept
+    {
+        return true;
+    }
+
+    static constexpr auto epsilon() noexcept
+    {
+        return false;
+    }
+
+    static constexpr auto round_error() noexcept
+    {
+        return false;
+    }
+
+    static constexpr auto infinity() noexcept
+    {
+        return false;
+    }
+
+    static constexpr auto quiet_NaN() noexcept
+    {
+        return false;
+    }
+
+    static constexpr auto signaling_NaN() noexcept
+    {
+        return false;
+    }
+
+    static constexpr auto denorm_min() noexcept
+    {
+        return false;
+    }
+};
+
 } // namespace std
 
 #endif // STD_LIMITS_H

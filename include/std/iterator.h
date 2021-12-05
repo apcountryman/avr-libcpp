dcc427659fb2f610b46c6d006f9ea46372916b3d
Andrew Countryman <apcountryman@gmail.com>
2021-12-05 12:36:05 -0800

    Add std::reverse_iterator
    Resolves #158 (Add std::reverse_iterator).

--- include/std/iterator.h
+++ include/std/iterator.h
@@ -24,6 +24,7 @@
 #define STD_ITERATOR_H
 
 #include <cstddef>
+#include <memory>
 #include <type_traits>
 
 namespace std {
@@ -85,6 +86,186 @@ struct iterator_traits<T const *> {
 
     using iterator_category = random_access_iterator_tag;
 };
+
+template<typename Iterator>
+class reverse_iterator {
+  public:
+    using iterator_type = Iterator;
+
+    using iterator_category = typename iterator_traits<iterator_type>::iterator_category;
+
+    using value_type = typename iterator_traits<iterator_type>::value_type;
+
+    using difference_type = typename iterator_traits<iterator_type>::difference_type;
+
+    using pointer = typename iterator_traits<iterator_type>::pointer;
+
+    using reference = typename iterator_traits<iterator_type>::reference;
+
+    constexpr reverse_iterator() = default;
+
+    constexpr explicit reverse_iterator( iterator_type iterator ) : current{ iterator }
+    {
+    }
+
+    template<typename Other_Iterator>
+    constexpr reverse_iterator( reverse_iterator<Other_Iterator> const & other_iterator ) :
+        current{ other_iterator.current }
+    {
+    }
+
+    template<typename Other_Iterator>
+    constexpr auto operator=( reverse_iterator<Other_Iterator> const & other_iterator )
+        -> reverse_iterator &
+    {
+        current = other_iterator.current;
+
+        return *this;
+    }
+
+    constexpr auto base() const -> iterator_type
+    {
+        return current;
+    }
+
+    constexpr auto operator*() const -> reference
+    {
+        auto iterator = current;
+
+        return *--iterator;
+    }
+
+    constexpr auto operator->() const -> pointer
+    {
+        return addressof( operator*() );
+    }
+
+    constexpr auto operator[]( difference_type offset ) const -> reference
+    {
+        return current[ -offset - 1 ];
+    }
+
+    constexpr auto operator++() -> reverse_iterator &
+    {
+        --current;
+
+        return *this;
+    }
+
+    constexpr auto operator--() -> reverse_iterator &
+    {
+        ++current;
+
+        return *this;
+    }
+
+    constexpr auto operator++( int ) -> reverse_iterator
+    {
+        auto iterator = current;
+
+        --current;
+
+        return iterator;
+    }
+
+    constexpr auto operator--( int ) -> reverse_iterator
+    {
+        auto iterator = current;
+
+        ++current;
+
+        return iterator;
+    }
+
+    constexpr auto operator+( difference_type n ) const -> reverse_iterator
+    {
+        return current - n;
+    }
+
+    constexpr auto operator-( difference_type n ) const -> reverse_iterator
+    {
+        return current + n;
+    }
+
+    constexpr auto operator+=( difference_type n ) -> reverse_iterator &
+    {
+        current -= n;
+
+        return *this;
+    }
+
+    constexpr auto operator-=( difference_type n ) -> reverse_iterator &
+    {
+        current += n;
+
+        return *this;
+    }
+
+  protected:
+    iterator_type current{};
+};
+
+template<typename Iterator_LHS, typename Iterator_RHS>
+constexpr auto operator==( reverse_iterator<Iterator_LHS> const & lhs, reverse_iterator<Iterator_RHS> const & rhs )
+    -> bool
+{
+    return lhs.base() == rhs.base();
+}
+
+template<typename Iterator_LHS, typename Iterator_RHS>
+constexpr auto operator!=( reverse_iterator<Iterator_LHS> const & lhs, reverse_iterator<Iterator_RHS> const & rhs )
+    -> bool
+{
+    return not( lhs == rhs );
+}
+
+template<typename Iterator_LHS, typename Iterator_RHS>
+constexpr auto operator<( reverse_iterator<Iterator_LHS> const & lhs, reverse_iterator<Iterator_RHS> const & rhs )
+    -> bool
+{
+    return lhs.base() > rhs.base();
+}
+
+template<typename Iterator_LHS, typename Iterator_RHS>
+constexpr auto operator>( reverse_iterator<Iterator_LHS> const & lhs, reverse_iterator<Iterator_RHS> const & rhs )
+    -> bool
+{
+    return lhs.base() < rhs.base();
+}
+
+template<typename Iterator_LHS, typename Iterator_RHS>
+constexpr auto operator<=( reverse_iterator<Iterator_LHS> const & lhs, reverse_iterator<Iterator_RHS> const & rhs )
+    -> bool
+{
+    return lhs.base() >= rhs.base();
+}
+
+template<typename Iterator_LHS, typename Iterator_RHS>
+constexpr auto operator>=( reverse_iterator<Iterator_LHS> const & lhs, reverse_iterator<Iterator_RHS> const & rhs )
+    -> bool
+{
+    return lhs.base() <= rhs.base();
+}
+
+template<typename Iterator>
+constexpr auto operator+( typename reverse_iterator<Iterator>::difference_type n, reverse_iterator<Iterator> const & iterator )
+    -> reverse_iterator<Iterator>
+{
+    return iterator.base() - n;
+}
+
+template<typename Iterator_LHS, typename Iterator_RHS>
+constexpr auto operator-( reverse_iterator<Iterator_LHS> const & lhs, reverse_iterator<Iterator_RHS> const & rhs )
+    -> decltype( rhs.base() - lhs.base() )
+{
+    return rhs.base() - lhs.base();
+}
+
+template<typename Iterator>
+constexpr auto make_reverse_iterator( Iterator iterator ) -> reverse_iterator<Iterator>
+{
+    return iterator;
+}
 
 } // namespace std
 
